c56205b2063458b4464aac620ffbc300579d343d
Modesto <modesto@goodmeal.app>
2025-05-14 10:37:21 -0400

    update placeholder

--- resources/pages/App.vue
+++ resources/pages/App.vue
@@ -24,7 +24,7 @@ const getWeather = async () => {
           <input
             v-model="city"
             type="text"
-            placeholder="Ej. Santa Cruz, BO"
+            placeholder="Ej. Madrid, ESP"
             class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500"
           />
           <button
